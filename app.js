2f24d85c0f24dbf957b3dbd72a22463964c3d6e9
hibatullahbjn-debug <pesantrenhibatullah@gmail.com>
2026-08-20 11:41:30 +0700

    Perbarui Template ID EmailJS menjadi template_65bp4xq

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const FONNTE_TOKEN = "Gp6kkhv5BtzLh49iw45o";
 
 // ---- EmailJS Configuration (Metode 1) ----
 const EMAILJS_SERVICE_ID = "service_pj21c0q";
-const EMAILJS_TEMPLATE_ID = "template_hjktqkq";
+const EMAILJS_TEMPLATE_ID = "template_65bp4xq";
 const EMAILJS_PUBLIC_KEY = "CInhHaoRzugLo_XSi";
 
 function getRoleEmail(targetRole) {
