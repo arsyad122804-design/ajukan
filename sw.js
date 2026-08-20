62163345fbd711e2f989a7b498759fd1c2fdf90e
hibatullahbjn-debug <pesantrenhibatullah@gmail.com>
2026-08-20 12:27:38 +0700

    Perbarui sw.js ke versi v2 untuk membersihkan cache service worker otomatis

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'spms-cache-v1';
+const CACHE_NAME = 'spms-cache-v2';
 const urlsToCache = [
   './',
   './login.html',
@@ -10,9 +10,25 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Clearing old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
